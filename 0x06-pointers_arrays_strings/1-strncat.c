87a3d0044e91891987c5f2106a6936dfacca3305
lucydevonne <wambuilucie7@gmail.com>
2022-09-22 10:26:22 -0700

    Write a function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,18 +4,6 @@
  *@dest: string to be appended upon
  *@src: string to be completed at end of dest
  *@n:integer parameter to compare index to
-#include "main.h"
-
-/**
-
- *_strncat - concatenate two strings but add inputted number of bytes
-
- *@dest: string to be appended upon
-
- *@src: string to be completed at end of dest
-
- *@n:integer parameter to compare index to
-
  *Return: returns new concatenated string
  */
 
