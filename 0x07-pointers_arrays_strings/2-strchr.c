42a6397b75c6f362cf6d18294fafdd0dedea3446
lucydevonne <wambuilucie7@gmail.com>
2022-09-27 12:25:36 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; *(s + i); i++)
-	{
-		if (*(s + i) == c)
-			return (s + i);
-	}
-	if (*(s + i) == c)
-		return (s + i);
-		return (s + i);
-	return (0);
+		for (i = 0; *(s + i); i++)
+				{
+							if (*(s + i) == c)
+											return (s + i);
+								}
+			if (*(s + i) == c)
+						return (s + i);
+				return (0);
 }
+
