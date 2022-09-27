3f7e151953ce0ac534f4ba60e5b4fde2a53b0773
lucydevonne <wambuilucie7@gmail.com>
2022-09-27 12:05:20 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,44 +1,20 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * s - memory space to be filled
- * c − This is the value to be set. The value is passed as an int, but the function fills the block of memory using the unsigned char conversion of this value.
- * n − This is the number of bytes to be set to the value.
+ * _memset - fills a memory block with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to be used
  *
- * Return : Nothing
+ * Return: pointer to the memory block
  */
 
-void *_memset (void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i ;
-
-	i = 0;
-	while (i < size)
+	while (n)
 	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
+		s[n - 1] = b;
+		n--;
 	}
-	printf("\n");
+	return (s);
 }
-
-int main (void)
-{
-	char buffer [98] = {0x00};
-
-	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
-	 printf("-------------------------------------------------\n");
-	 simple_print_buffer(buffer, 98); 
-	 return (0);
-}
-
