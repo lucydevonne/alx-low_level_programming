0fde29bc164612a7aa4d182a4f57e3a1517891b0
lucydevonne <wambuilucie7@gmail.com>
2022-09-30 11:42:35 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -42,33 +42,33 @@ int main(int argc, char *argv[])
 
 {
 
-	/*Declaring variables*/
-	int count;
-	int str_to_int;
-	int sum = 0;
+/*Declaring variables*/
+int count;
+int str_to_int;
+int sum = 0;
 
-	count = 1;
-	while (count < argc) /*Goes through the whole array*/
-	{
-	if(check_num(argv[count]))
+count = 1;
+while (count < argc) /*Goes through the whole array*/
+{
+if(check_num(argv[count]))
 
-	{
-	str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-	sum += str_to_int;
-	}
+{
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
+}
 
-	/*Condition if one of the number contains symbols that are not digits*/
-	else
-	{
-	printf("Error\n");
-	return (1);
-	}
+/*Condition if one of the number contains symbols that are not digits*/
+else
+{
+printf("Error\n");
+return (1);
+}
 
-	count++;
-	}
+count++;
+}
 
-	printf("%d\n", sum); /*print sum*/
+printf("%d\n", sum); /*print sum*/
 
-	return (0);
+return (0);
 }
 
