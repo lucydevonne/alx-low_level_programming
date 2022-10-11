80ea3f99ac7ea23ee84fcc11988d4688447b7851
lucydevonne <wambuilucie7@gmail.com>
2022-10-11 16:11:34 -0700

    object like macro

--- 0x0E-structures_typedef/0x0D-preprocessor/0-object_like_macro.h
+++ 0x0E-structures_typedef/0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _O_OBJECT_LIKE_MACRO_H_
-#define _O_OBJJECT_LIKE_MACRO_H_
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJJECT_LIKE_MACRO_H_
 
 #define SIZE 1024
 
-#endif /* _O_OBJECT_LIKE_MACRO_H_ */
+#endif /* _0_OBJECT_LIKE_MACRO_H_ */
