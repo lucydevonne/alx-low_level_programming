d0756e7e928dd24327363dfa64fa66d412932c85
lucydevonne <wambuilucie7@gmail.com>
2023-04-12 23:41:48 -0700

    search alg

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
+#include "search_algos.h"
 
 int linear_search(int *array, size_t size, int value) {
-    for (size_t i = 0; i < size; i++) {
-        if (array[i] == value) {
+	size_t i;
+		if (array==NULL)
+			return (-1);
+			    
+		for (size_t i = 0; i < size; i++) {
+        		if (array[i] == value) {
             printf("Found %d at index %zu\n", value, i);
-            return i;
+            return (i);
         }
     }
-    printf("%d not found in the array\n", value);
-    return -1;
+    return (-1);
 }
