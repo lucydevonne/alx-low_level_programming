a91a36dbbb97bf5d4babc7ea3e5259a444a380da
lucydevonne <wambuilucie7@gmail.com>
2023-04-13 00:50:55 -0700

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,12 +1,24 @@
-#include <stdio.h>
+#include "search_algos.h"
 
 int binary_search(int *array, size_t size, int value); {
-	for (size_t i = 0; i < size; i++) {
-        if (array[i] == value) {
-            printf("Found %d at index %zu\n", value, i);
-            return i;
-        }
-    }
-    printf("%d not found in the array\n", value);
-    return -1;
+	
+	size_t i;
+	
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+{
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+
+	i = left + (right - left) / 2;
+	if (array[i] == value)
+		return (i);
+	if (array[i] > value)
+		right = i - 1;
+	else
+		left = i + 1;
 }
